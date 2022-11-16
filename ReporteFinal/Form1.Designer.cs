cb6c4931cbbc30e2c4ea01065667eb6f09130996
GonzaFantini <gonzafantini@gmail.com>
2022-11-16 16:26:28 -0300

    gonzalo

---
+++ ReporteFinal/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace ReporteFinal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.dSListado = new ReporteFinal.Reporte.DSListado();
+            this.dTListadoBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dTListadoTableAdapter = new ReporteFinal.Reporte.DSListadoTableAdapters.DTListadoTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dSListado)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dTListadoBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource1.Name = "DataSet1";
+            reportDataSource1.Value = this.dTListadoBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "ReporteFinal.Reporte.ListadoTickets.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(800, 450);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // dSListado
+            // 
+            this.dSListado.DataSetName = "DSListado";
+            this.dSListado.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // dTListadoBindingSource
+            // 
+            this.dTListadoBindingSource.DataMember = "DTListado";
+            this.dTListadoBindingSource.DataSource = this.dSListado;
+            // 
+            // dTListadoTableAdapter
+            // 
+            this.dTListadoTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dSListado)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dTListadoBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private Reporte.DSListado dSListado;
+        private System.Windows.Forms.BindingSource dTListadoBindingSource;
+        private Reporte.DSListadoTableAdapters.DTListadoTableAdapter dTListadoTableAdapter;
+    }
+}
+
